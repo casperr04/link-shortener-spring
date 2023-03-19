b8515da6f3f3f312725a1dbe1f9a0d8b40ccca66
Kacper <mikrel071204@gmail.com>
2023-03-19 21:10:28 +0100

    init

---
+++ src/test/java/com/kacper/linkshortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacper.linkshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
