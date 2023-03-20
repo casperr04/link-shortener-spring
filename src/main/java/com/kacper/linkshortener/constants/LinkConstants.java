1a2b5a8c71fda816ae4fce5d05ef16ae6c1a0d67
Kacper <mikrel071204@gmail.com>
2023-03-21 00:31:40 +0100

    Created constant for entity expiration

--- src/main/java/com/kacper/linkshortener/constants/LinkConstants.java
+++ src/main/java/com/kacper/linkshortener/constants/LinkConstants.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 public class LinkConstants {
     private final String CONTROLLER_PREFIX = "li";
     private final String CONTROLLER_SUFFIX = "k";
+    private final int ENTITY_EXPIRATION_DAYS = 5;
 
     public String getCONTROLLER_PREFIX() {
         return CONTROLLER_PREFIX;
