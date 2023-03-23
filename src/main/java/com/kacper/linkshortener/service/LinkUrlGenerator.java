e8344aff1ab31c33c4d413d4adea407a2daa3a65
Kacper <mikrel071204@gmail.com>
2023-03-23 21:07:11 +0100

    Service documentation

--- src/main/java/com/kacper/linkshortener/service/LinkUrlGenerator.java
+++ src/main/java/com/kacper/linkshortener/service/LinkUrlGenerator.java
@@ -1,7 +1,18 @@
 package com.kacper.linkshortener.service;
 
 public interface LinkUrlGenerator {
+
+    /**
+     * Generates a random ID given length.
+     * @param length Length of the ID to be generated.
+     * @return Generated ID string.
+     */
     String generateRandomID(int length);
 
+    /**
+     * Generates a random String given length.
+     * @param length Length of the String to be generated.
+     * @return Generated string.
+     */
     String generateRandomString(int length);
 }
--- src/main/java/com/kacper/linkshortener/service/LinkValidator.java
+++ src/main/java/com/kacper/linkshortener/service/LinkValidator.java
@@ -3,12 +3,15 @@ package com.kacper.linkshortener.service;
 public interface LinkValidator {
 
     /**
-     * Checks if the passed link is a valid link.
+     * Checks if the passed link is a valid link
+     * @return true or false, depending if link is valid.
      */
     boolean validateLink(String link);
 
     /**
      * Verifies that the given redirect link ID is unique.
+     * @param link ID to test, will be returned if unique.
+     * @return An unique ID.
      */
     String uniqueLinkIDValidator(String link);
 }
