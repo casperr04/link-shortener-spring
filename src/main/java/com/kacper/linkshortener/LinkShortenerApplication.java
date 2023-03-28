f299c14af1b00d9d8f4e25e931729ff0ee22a248
Kacper <mikrel071204@gmail.com>
2023-03-28 13:55:58 +0200

    Removed automatic password generation

--- src/main/java/com/kacper/linkshortener/LinkShortenerApplication.java
+++ src/main/java/com/kacper/linkshortener/LinkShortenerApplication.java
@@ -2,12 +2,12 @@ package com.kacper.linkshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableScheduling
 public class LinkShortenerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LinkShortenerApplication.class, args);
     }
