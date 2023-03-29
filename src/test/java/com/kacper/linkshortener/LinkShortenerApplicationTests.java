245600f8a381be7c8f7ffa695ec317915d8c83b6
Kacper <mikrel071204@gmail.com>
2023-03-29 21:24:25 +0200

    GitHub Actions potential test fix

--- src/test/java/com/kacper/linkshortener/LinkShortenerApplicationTests.java
+++ src/test/java/com/kacper/linkshortener/LinkShortenerApplicationTests.java
@@ -1,9 +1,11 @@
 package com.kacper.linkshortener;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisabledIfEnvironmentVariable(named = "CI", matches = "true")
 class LinkShortenerApplicationTests {
 
     @Test
